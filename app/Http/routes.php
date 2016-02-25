22e07d08f0b306d87534537a2a9acfd2d8f21efe
lisundev@gmail.com <lisundev@gmail.com>
2016-02-25 20:02:20 +0800

    Signed-off-by: lisundev@gmail.com <lisundev@gmail.com>

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,6 +2,8 @@
 
 Route::get('/', 'HomeController@index');
 
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
@@ -9,4 +11,8 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 Route::group(['prefix'=>'admin', 'namespace'=>'Admin', 'middleware' => 'auth'], function()
 {
 	Route::get('/', 'AdminHomeController@index');
+	Route::resource('pages', 'PagesController');
+	Route::resource('comments', 'CommentsController');
 });
+
+Route::post('comment/store', 'CommentsController@store');
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,11 @@
 @extends('_layouts.app')
 
 @section('content')
-<div class="container-fluid">
+
+<div class="container">
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
+			<div class="login-panel panel panel-default">
 				<div class="panel-heading">Register</div>
 				<div class="panel-body">
 					@if (count($errors) > 0)
